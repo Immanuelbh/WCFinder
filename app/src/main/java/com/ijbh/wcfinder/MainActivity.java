22f0027f1fd570203bd5c42bb7f250a53aafc6c1
Immanuelbh <37409984+Immanuelbh@users.noreply.github.com>
2019-11-13 19:08:08 +0200

    added new WC activity and initial layout

--- app/src/main/java/com/ijbh/wcfinder/MainActivity.java
+++ app/src/main/java/com/ijbh/wcfinder/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(MainActivity.this, NewWc.class);
+                startActivity(intent);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
@@ -62,5 +77,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         recyclerView.setAdapter(waterClosetAdapter);
+
+
     }
 }
---
+++ app/src/main/java/com/ijbh/wcfinder/NewWc.java
@@ -0,0 +1,14 @@
+package com.ijbh.wcfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewWc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_wc);
+    }
+}
